9ace6c0927f6c8b08837ac1cc947f2f6efbd3502
Bhaskar Thakur <48211637+BhaskarThakur1997@users.noreply.github.com>
2019-10-29 13:31:49 +0530

    Add files via upload

---
+++ ATMMachine.java
@@ -0,0 +1,15 @@
+
+package atm.machine;
+
+import java.io.IOException;
+
+public class ATMMachine extends OptionMenu{
+
+    public static void main(String[] args)throws IOException {
+        // TODO code application logic here
+   
+OptionMenu optionMenu = new OptionMenu();
+    optionMenu.getLogin();
+    }
+    
+}
